6f43affca4271fdc62d134fa8a84da18ec6fb873
mike09 <mike09@mail.ru>
2022-10-03 18:19:49 +0300

    add model and mock repository

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,6 +1,10 @@
+using CommandAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
 
 var app = builder.Build();
 
