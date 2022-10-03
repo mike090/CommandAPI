b6d101a1870a80897dadb8f268f414e2bc860fae
mike09 <mike09@mail.ru>
2022-10-03 23:30:49 +0300

    add CommandContext

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,10 +1,15 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+
+builder.Services.AddDbContext<CommandContext>(opts => opts.UseNpgsql(
+    builder.Configuration["ConnectionStrings:PostgresSqlConnection"]
+));
 
 var app = builder.Build();
 
